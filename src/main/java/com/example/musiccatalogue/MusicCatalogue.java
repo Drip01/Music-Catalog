4ec79230480badc6fc2ae13a34905df5f67fd1c9
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-07-11 11:09:28 +0100

    readme

--- src/main/java/com/example/musiccatalogue/MusicCatalogue.java
+++ src/main/java/com/example/musiccatalogue/MusicCatalogue.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MusicCatalogue extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("music_catalog.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(MusicCatalogue.class.getResource("music_catalog.fxml"));
 		Scene scene = new Scene(fxmlLoader.load());
 		stage.setTitle("Music Catalogue");
 		stage.setResizable(false);
